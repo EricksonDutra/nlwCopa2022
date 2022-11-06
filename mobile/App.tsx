cf6861c69f0388abdc6d92e8e0bacbb9d908a905
Erickson Dutra <ericksond10@gmail.com>
2022-11-06 16:45:05 -0400

    mobile screens

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,26 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
-import { Loading } from './src/components/loading';
-import { SignIn } from './src/screens/SignIn';
-
-
+import { Loading } from "./src/components/Loading";
 import { THEME } from './src/styles/theme';
+
+import { SignIn } from "./src/screens/SignIn";
+
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
 
   const [fontsLoaded] = useFonts ({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
       <StatusBar
       barStyle="light-content"
       backgroundColor="transparent"
       translucent
       />
         { fontsLoaded ? <SignIn/> : <Loading/> }
+        </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
